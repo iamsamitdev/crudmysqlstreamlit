5f75eaa6acee01d283e97dd95765917bb407f87f
Samit Koyom <samitkoyom@gmail.com>
2023-09-20 15:33:17 +0700

    update connnect mysql file

--- connectmysql.py
+++ connectmysql.py
@@ -2,11 +2,22 @@ import pymysql
 
 # สร้างฟังก์ชันสำหรับเชื่อมต่อฐานข้อมูล MySQL
 def connectdb():
+    # local
+    # connection = pymysql.connect(
+    #     host='localhost',
+    #     user='root',
+    #     password='1234',
+    #     db='pythondb',
+    #     port=3306,
+    #     cursorclass=pymysql.cursors.DictCursor
+    # )
+
+    # server
     connection = pymysql.connect(
-        host='localhost',
-        user='root',
-        password='1234',
-        db='pythondb',
+        host='b3palitjkq6y142h9y0v-mysql.services.clever-cloud.com',
+        user='uh7ciudmmxi9gdnp',
+        password='L09BGgFWKOzvl9ktggQZ',
+        db='b3palitjkq6y142h9y0v',
         port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
